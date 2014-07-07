383f3b68d267caf2115e02e39085cdb3555f332a
rongself <rongself1991@gmail.com>
2014-07-07 23:28:21 +0800

    Initial commit

---
+++ js/simple-app.js
@@ -0,0 +1,26 @@
+$(function(){
+
+	//Function to Fix Pages Height
+	function fixPagesHeight() {
+		$('.swiper-pages').css({
+			height: $(window).height()
+		})
+        var wheight = $(window).height();
+        var wwidth = $(window).width();
+        $('img.content-img').width(wwidth).height(wheight);
+	}
+	$(window).on('resize',function(){
+		fixPagesHeight()
+	})
+	fixPagesHeight()
+
+	//Init Pages
+	var pages = $('.swiper-container').swiper({
+        mode: 'vertical',
+        freeMode: false,
+        freeModeFluid: false
+    });
+
+
+
+})
