13b8dbe90af9a8b22a717fd417017473f087d5ec
Ron <roncai@crazysales.com.au>
2014-08-13 14:30:59 +0800

    some improvement

--- js/simple-app.js
+++ js/simple-app.js
@@ -1,3 +1,9 @@
+var device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|micromessenger/i.test(navigator.userAgent.toLowerCase()));
+
+if(!device){
+    alert('请用手机浏览器浏览已得到最佳效果');
+}
+
 $(function(){
 
 	//Function to Fix Pages Height
@@ -40,5 +46,22 @@ $(function(){
             bgm.pause();
         }
     },false);
+
+
+    var lottery = new Lottery('lotteryContainer', 'img/mask.jpg', 'image', $(window).width(),$(window).height(),
+        function (percent) {
+            if(percent>=60){
+                $('#paint').fadeOut(300,function(){
+                    $(this).remove();
+                    bgm.play();
+                });
+            }
+        }
+    );
+    lottery.init('img/gallery-2.jpg', 'image');
+
+    $('#stage').bind('maskloaded',function(){
+        $('#loading').fadeOut(500);
+    });
 
 })
