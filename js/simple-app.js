7fd77f84f41afc10528e99d1e436354672443e57
rongself <rongself1991@gmail.com>
2015-02-15 01:49:54 +0800

    new version update

--- js/simple-app.js
+++ js/simple-app.js
@@ -5,102 +5,72 @@ if(!device){
 }
 
 $(function(){
-
-	//Function to Fix Pages Height
-	function fixPagesHeight() {
-		$('.swiper-pages').css({
-			height: $(window).height()
-		})
-        var wheight = $(window).height();
-        var wwidth = $(window).width();
-        //$('img.content-img').width(wwidth).height(wheight);
-	}
-	$(window).on('resize',function(){
-		fixPagesHeight()
-	})
-	fixPagesHeight()
-
+    var images = document.getElementsByClassName('bg-img');
 	//Init Pages
     var  imageLazyLoader = function(){
         var self = this;
-        self.load = function(ele){
-            var url = $(ele).attr('data-img-src');
-            if(url){
-                if($(ele).css('background-image')=='none'){
-                    var loadingHtml = '<div id="loading" style="background-color: transparent;">\
+        self.loadCount = 0;
+        self.load = function(eles){
+            for(var i =0 ;i<eles.length;i++){
+                var url = $(eles[i]).attr('data-img-src');
+                if(url){
+                    if($(eles[i]).css('background-image')=='none'){
+                        $(document).trigger('imgBeforeLoading',[eles[i]]);
+
+                        //load image and set as background
+                        $('<img/>').on('load',eles[i],function(e){
+                            self.loadCount ++;
+                            $(document).trigger('imgLoading',[self.loadCount, e.data]);
+                            $(e.data).trigger('imgLoad');
+                        }).attr('src',url);
+
+                    }
+                }else{
+                    throw 'this div has not "data-img-src" attr';
+                }
+            }
+        }
+
+        self.preLoad = function(eles,preLoadCount){
+            preLoadCount = preLoadCount||2;
+            for(var i =0 ;i<eles.length;i++){
+                self.load(eles[i]);
+            }
+        }
+    }
+
+    $(document).on('imgBeforeLoading',function(e,element){
+        //handle load animate
+        var loadingHtml = '<div id="loading" style="background-color: transparent;">\
                                 <div class="spinner">\
                                 <div class="dot1"></div>\
                                 <div class="dot2"></div>\
                                 </div>\
-                                </div>';
-                    if(!$(ele).has('#loading').length > 0){
-                        var loading = $(loadingHtml).appendTo(ele);
-                    }
-                    $('<img/>').on('load',function(){
-                        $(ele).css('background-image','url("'+url+'")').hide().fadeIn(300).trigger('imgload');
-                        if(loading&&loading.length > 0){
-                            loading.remove();
-                        }
-                    }).attr('src',url);
-
-                }
-            }else{
-                throw 'this div has not "data-img-src" attr';
-            }
+                            </div>';
+        if(!$(element).has('#loading').length > 0){
+            var loading = $(loadingHtml).appendTo(element);
         }
-    }
-    var loader = new imageLazyLoader();
-    loader.load($('.swiper-wrapper .bg-img').eq(0));
-    loader.load($('.swiper-wrapper .bg-img').eq(1));
-	var pages = $('.swiper-container').swiper({
-        mode: 'vertical',
-        freeMode: false,
-        freeModeFluid: false,
-        onSlideChangeStart:function(item){
-            var count = item.slides.length;
-            var index = item.activeIndex;
-            //console.log(item);
-            if(index+1<count){
-                loader.load($(item.slides[index+1]).children('div'));
-            }
-
-            if(index == $('.bg-img').length-1){
-                $('.up').fadeOut(300);
-            }else{
-                if(!$('.up').is(':visible')){
-                    $('.up').fadeIn(300);
-                }
-            }
-
-
-        }
-
     });
 
-    document.getElementsByClassName('touch-layer')[0].addEventListener('touchstart',function(){
-        if(bgm.paused){
-            $(this).children('#soundControl').addClass('jump');
-            bgm.play();
-        }else{
-            $(this).children('#soundControl').removeClass('jump');
-            bgm.pause();
+    $(document).on('imgLoading',function(e,count,element){
+        var url = $(element).attr('data-img-src');
+
+        $(element).css('background-image','url("'+url+'")').hide().fadeIn(300);
+        var loading = $(element).children('#loading');
+        if(loading&&loading.length > 0){
+            loading.remove();
         }
-    },false);
-
-
-    var lottery = new Lottery('lotteryContainer', 'img/mask.jpg', 'image', $(window).width(),$(window).height(),
-        function (percent) {
-            if(percent>=60){
-                $('#paint').fadeOut(300,function(){
-                    $(this).remove();
-                    bgm.play();
-                });
-            }
+        $('#loadingCount').text(parseInt((count/images.length)*100)+'%');
+        if(count>=images.length){
+            $('#loading').fadeOut(500);
         }
-    );
-    lottery.init('img/1.jpg', 'image');
+    });
 
-    $('#stage').bind('maskloaded',function(){
-        $('#loading').fadeOut(500);
+    var loader = new imageLazyLoader();
+    loader.load(images);
+    var pages = $('.swiper-container').swiper({
+        mode: 'vertical',
+        freeMode: false,
+        freeModeFluid: false
     });
 })
